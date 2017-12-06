4d675a7c93d77d8943564904ea3b0cba9d9556e4
Heraldo Yusron <heraldoyusrontris@gmail.com>
2017-12-06 10:32:33 +0700

    nambah role

--- routes/user.js
+++ routes/user.js
@@ -15,7 +15,7 @@ router.set('view engine', 'ejs');
 //     res.send("ini user");
 // })
 
-//                                         ---- Display
+//                                        ---- Display
 router.get('/users', (req, res)=>{
     models.User.findAll()
     .then(users =>{
@@ -24,15 +24,67 @@ router.get('/users', (req, res)=>{
     })
     .catch(error =>{
         console.log(error);
-        res,send(error)
+        res.send(error)
     })
 })
 //                                         ---- Create
 router.get('/users/add', (req, res)=>{
-    let obj = {
-        
-    }
+    res.render('userForm')
 })
-//                                         ---- Edit
-//                                         ---- Delete
+router.post('/users/add', (req, res)=>{
+    let obj = {
+        username: req.body.username,
+        password: req.body.password,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }
+    models.User.create(obj)
+    .then(data=>{
+        // console.log(obj)
+        res.redirect('/users');
+    })
+    .catch(error=>{
+        console.log(error);
+        res.send(error);
+    })
+})
+//                                        ---- Edit
+router.get('/users/edit/:id', (req, res) =>{
+    models.User.findById(req.params.id)
+    .then(data=>{
+        res.render('userEdit', {data: data})
+    })
+    .catch(error=>{
+        console.log(error)
+        res.send(error);
+    })
+})
+router.post('/users/edit/:id', (req, res)=>{
+    let obj = {
+        username: req.body.username,
+        password: req.body.password,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }
+    models.User.update(obj, {where: {id: req.params.id}})
+    .then(data=>{
+        res.redirect('/users')
+    })
+    .catch(error=>{
+        console.log(error)
+        res.send(error)
+    })
+})
+//                                        ---- Delete
+router.get('/users/delete/:id', (req, res)=>{
+    models.User.destroy({where: {id:req.params.id}})
+    .then(data=>{
+        res.redirect('/users');
+    })
+    .catch(error=>{
+        console.log(error)
+        res.send(error);
+    })
+})
+
 router.listen(3000);
