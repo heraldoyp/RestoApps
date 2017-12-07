092cc94d404030e2e546ec3f9f42e859eb306ce8
rezaalfarabi <anangreza18@gmail.com>
2017-12-07 18:32:41 +0700

    session dan hash

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const bodyParser = require('body-parser')
 const session = require('express-session')
 const bcrypt = require('bcrypt');
 app.use(session({
-    secret: 'jossgandosgraotos'
+    secret: 'success'
   }))
 
 const index         = require('./routes/index');
 const menu          = require('./routes/menu');
 const register      = require('./routes/register')
 const login         = require('./routes/login')
+const loginAdmin    = require('./routes/admin')
+const registerAdmin    = require('./routes/registerAdmin')
 
 // app.use(express.static(__dirname + '/views'))
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -22,8 +24,10 @@ app.set('view engine', 'ejs')
 
 app.use('/login',login)
 app.use('/register',register)
+app.use('/registerAdmin',registerAdmin)
 app.use('/index', index)
 app.use('/menus', menu)
+app.use('/loginAdmin', loginAdmin)
 
 
-app.listen(3000, () => console.log('lanjutttt!!!!'))
+app.listen(3000, () => console.log('lanjutttt!!!'))
--- models/admin.js
+++ models/admin.js
@@ -1,13 +1,13 @@
 'use strict';
 const bcrypt = require('bcrypt');
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
+  var Admin = sequelize.define('Admin', {
     username: {
       type: DataTypes.STRING,
       validate: {
         isUnique: function(value, callback) {
           console.log(value);
-          User.find({
+          Admin.find({
             where: {username: value}
           }).then((result) =>{
             if(result && this.id != result.id) {
@@ -23,13 +23,13 @@ module.exports = (sequelize, DataTypes) => {
     password: DataTypes.STRING
   });
 
-  User.beforeCreate((user, options) => {
+  Admin.beforeCreate((admin, options) => {
   const saltRounds = 10;
-  const myPlaintextPassword = user.password;
+  const myPlaintextPassword = admin.password;
   return  bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
-    user.password = hash
+    admin.password = hash
   });
 });
 
-  return User;
+  return Admin;
 };
